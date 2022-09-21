0f12f0ac55691cf544767f5e2950b6e1975702c4
KD <39858155+khduyentr@users.noreply.github.com>
2022-09-21 09:59:48 +0700

    basically build UI of note area

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
+
 import './App.css';
 import NoteArea from './NoteArea/NoteArea';
 import Sidebar from './Sidebar/Sidebar';
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
   return (
     <div className="App">
       <Sidebar/>
--- src/NoteArea/NoteArea.js
+++ src/NoteArea/NoteArea.js
@@ -1,16 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import {faOutdent, faList, faCircleInfo, faEllipsis, faEyeSlash, faEye} from '@fortawesome/free-solid-svg-icons'
 
 import styles from './NoteArea.module.css'
 
-
+// for the markup part
+{/* <FontAwesomeIcon icon={faEye} className={styles.noteAreaIcon}/>
+<FontAwesomeIcon icon={faEyeSlash} className={styles.noteAreaIcon}/> */}
 
 function NoteArea() {
     return (
         <div className={styles.noteArea}>
 
-            <div className="note-area__header">
-                
+            <div className={styles.noteAreaHeader}>
+                <div>
+                    <FontAwesomeIcon icon={faOutdent} className={styles.noteAreaIcon}/>
+                </div>
+
+                <div className={styles.noteAreaHeaderRight}>
+                    <FontAwesomeIcon icon={faList} className={styles.noteAreaIcon}/>
+                    <FontAwesomeIcon icon={faCircleInfo} className={styles.noteAreaIcon}/>
+                    <FontAwesomeIcon icon={faEllipsis} className={styles.noteAreaIcon}/>
+                </div>   
+            </div>
+
+            <div className={styles.noteContentArea}>
+                <textarea className={styles.noteWritingArea}/>
+                <input type="text" 
+                       placeholder="Add tag..."
+                       className={styles.noteTagArea}/>
+            </div>
+
+            <div className={styles.notePreviewArea}>
+                <h3 className={styles.notePreviewTitle}>TITLE</h3>
+                <div className={styles.notePreviewContent}>
+                    note preview
+                </div>
             </div>
 
         </div>
